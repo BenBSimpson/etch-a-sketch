f2494840749f30ca2477143abc2eca8a9760c11b
Ben Simpson <benjaminbrette@gmail.com>
2017-08-28 11:49:05 -0400

    Add README.md. Updatedindex.html, script.js, style.css. Page appears functional, no bugs yet found.

---
+++ README.md
@@ -0,0 +1,10 @@
+This project is part of the Web Development coursework at The Odin Project. 
+
+The intent is to create an "Etch-a-Sketch" style drawing program. The requirements listed were as follows:
+
+-Create a gitHub repository for the project.
+-Use javascript to create a grid of squares.
+	-The grid begins at 16 x 16 squares.
+	-Add jQuery to the file if necessary.
+	-Set up an effect that causes the squares to change color as the mouse cursor passes over them.
+-Create a reset button at the top of the page that clears the current drawing and allows the user to change the size of the squares withouth affecting the 	total size of the drawing area. 
--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 var canvasWidth = 16;
 var canvasHeight = 16;
+var $options = $("#options-button");
+var $reset = $("#reset-button");
 
 //buildCanvas function creates a grid on pageload as well as when appropriate buttons are clicked.
 var buildCanvas = function() {
-  for (h = 1; h < canvasHeight; h++) {
+  for (h = 1; h <= canvasHeight; h++) {
     $("#canvas").append('<div class="row"></div>');
   };
-  for (w = 1; w < canvasWidth; w++) {
+  for (w = 1; w <= canvasWidth; w++) {
     $('.row').append('<div class="unit"></div>');
   };
-  $("#canvas").css("width", (10 * canvasWidth) + "px").css("margin", "0 auto");
+  $('.unit').css('height', 600 / canvasHeight);
+  $('.unit').css('width', 600 / canvasWidth);
 };
 
 var colorUnit = function() {
@@ -21,4 +24,13 @@ var colorUnit = function() {
 $(document).ready(function() {
   buildCanvas();
   colorUnit();
+  $('#reset-button').on('click', function() {
+  	canvasHeight = prompt('Please enter a canvas size', 'Size');
+  	canvasWidth = canvasHeight;
+  	$('.unit').remove();
+  	$('.row').remove();
+  	buildCanvas();
+  	colorUnit();
+  });
+
 });
